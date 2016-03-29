96458c72f0d82f679adff9c18696832c3f75d435
Roman Divotkey <roman@divotkey.net>
2016-03-29 18:09:35 +0200

    Added info class.

---
+++ Simple Game Library/src/at/fhooe/mtd/sgl/Info.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Roman Divotkey, Univ. of Applied Sciences Upper Austria. 
+ * All rights reserved.
+ *  
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE', which is part of this source code package.
+ *  
+ * THIS CODE IS PROVIDED AS EDUCATIONAL MATERIAL AND NOT INTENDED TO ADDRESS
+ * ALL REAL WORLD PROBLEMS AND ISSUES IN DETAIL.
+ *******************************************************************************/
+package at.fhooe.mtd.sgl;
+
+public class Info {
+
+    public static final String NAME = "Simple Game Library";
+    
+    public static final String VERSION = "1.0.0";
+    
+    public static final String COPYRIGHT_HOLDER 
+        = "Roman Divotkey, Univ. of Applied Sciences Upper Austria.";
+    
+    public static final String COPYRIGHT_YEAR = "2016";
+        
+    public static final String FULL_NAME = NAME + " " + VERSION;
+    
+    public static final String COPYRIGHT = "Copyright (c) " + COPYRIGHT_YEAR
+            + COPYRIGHT_HOLDER + "." + " All rights reserved.";
+}
