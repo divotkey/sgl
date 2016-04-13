670bb6e251caf1f24ab18268670b54670fcf353a
Roman Divotkey <roman@divotkey.net>
2016-04-13 18:41:40 +0200

    Updated version information.

--- Simple Game Library/src/at/fhooe/mtd/sgl/Info.java
+++ Simple Game Library/src/at/fhooe/mtd/sgl/Info.java
@@ -14,7 +14,7 @@ public class Info {
 
     public static final String NAME = "Simple Game Library";
     
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.0";
     
     public static final String COPYRIGHT_HOLDER 
         = "Roman Divotkey, Univ. of Applied Sciences Upper Austria.";
