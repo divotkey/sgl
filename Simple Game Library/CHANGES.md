774f90b516d704cc74d25fc429921a536ee6d845
Roman Divotkey <roman@divotkey.net>
2016-04-29 08:21:17 +0200

    Added determineCommandKey to input utilities.

--- Simple Game Library/CHANGES.md
+++ Simple Game Library/CHANGES.md
@@ -3,6 +3,7 @@
 ## Version 1.3.0
 Date: *2016-04-??*
 
+- Added utility method `determineCommandKey` to get the command key for the current operating system.
 - Improved input system. The `keyDown` method provides in addition to the key code the corresponding character.
 - Added utility class to test if a character is printable.
 - Added more JavaDoc comments.
--- Simple Game Library/src/at/fhooe/mtd/sgl/input/Util.java
+++ Simple Game Library/src/at/fhooe/mtd/sgl/input/Util.java
@@ -10,14 +10,47 @@
  *******************************************************************************/
 package at.fhooe.mtd.sgl.input;
 
+import java.awt.Event;
+import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 
+/**
+ * A collection of input related utility methods.
+ */
 public class Util {
     
+    /**
+     * Returns if the specified character is a printable character.
+     * 
+     * @param c
+     *            the character to test
+     * @return {@code true} if the character is printable
+     */
     public static boolean isPrintableChar(char c) {
         Character.UnicodeBlock block = Character.UnicodeBlock.of(c);
         return (!Character.isISOControl(c)) && c != KeyEvent.CHAR_UNDEFINED
                 && block != null && block != Character.UnicodeBlock.SPECIALS;
     }
     
+    /**
+     * Determines the system specific command key. On Windows and Unix systems
+     * this will be the {@code VK_CONTROL} and {@code VK_META} on MacOS.
+     * 
+     * @return the key code of the command key
+     */
+    public static int determineCommandKey() {
+        switch (Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()) {
+        case Event.META_MASK:
+            return KeyEvent.VK_META;
+
+        case Event.CTRL_MASK:
+            return KeyEvent.VK_CONTROL;
+
+        case Event.ALT_MASK:
+            return KeyEvent.VK_ALT;
+
+        default:
+            return KeyEvent.VK_CONTROL;
+        }
+    }    
 }
