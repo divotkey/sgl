5985f9b59edc4fb294349abbbf1ceb8367f4baf8
Roman Divotkey <roman.divotkey@gmail.com>
2018-03-31 17:05:47 +0200

    Updated copyright header.

--- Simple Game Library/src/at/fhooe/mtd/sgl/math/Quaternion.java
+++ Simple Game Library/src/at/fhooe/mtd/sgl/math/Quaternion.java
@@ -1,10 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2016 Roman Divotkey, Univ. of Applied Sciences Upper Austria. 
+ * Copyright (c) 2016 - 2018 Roman Divotkey,
+ * Univ. of Applied Sciences Upper Austria. 
  * All rights reserved.
- *  
+ *   
  * This file is subject to the terms and conditions defined in file
  * 'LICENSE', which is part of this source code package.
- *  
+ *    
  * THIS CODE IS PROVIDED AS EDUCATIONAL MATERIAL AND NOT INTENDED TO ADDRESS
  * ALL REAL WORLD PROBLEMS AND ISSUES IN DETAIL.
  *******************************************************************************/
