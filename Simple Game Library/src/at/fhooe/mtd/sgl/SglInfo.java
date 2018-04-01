6f8ca47c3605f1cb5c2bf6352e41fa38446fc823
Roman Divotkey <roman.divotkey@gmail.com>
2018-04-01 12:44:23 +0200

    Added JavaDoc comments.

--- Simple Game Library/src/at/fhooe/mtd/sgl/SglInfo.java
+++ Simple Game Library/src/at/fhooe/mtd/sgl/SglInfo.java
@@ -11,19 +11,29 @@
  *******************************************************************************/
 package at.fhooe.mtd.sgl;
 
+/**
+ * Provides version information of SGL.
+ */
 public class SglInfo {
 
+	/** The name of this library. */
     public static final String NAME = "Simple Game Library";
-    
+
+    /** The version number as string. */
     public static final String VERSION = "1.6.0";
     
+    /** The copyright holder of this version. */
     public static final String COPYRIGHT_HOLDER 
         = "Roman Divotkey, Univ. of Applied Sciences Upper Austria";
     
+    /** The copyright year. */
     public static final String COPYRIGHT_YEAR = "2016 - 2018";
         
+    /** The name plus version information. */
     public static final String FULL_NAME = NAME + " (" + VERSION + ")";
     
+    /** The complete copyright string. */
     public static final String COPYRIGHT = "Copyright (c) " + COPYRIGHT_YEAR
             + COPYRIGHT_HOLDER + "." + " All rights reserved.";
+    
 }
